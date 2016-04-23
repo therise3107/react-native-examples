89f8156e0c0dfdfbda2cf2962685df565213d830
Lalit Yadav <lyadav3107@gmail.com>
2016-04-23 09:49:26 +0530

    Update singin.js

--- authenticaition/src/components/singin.js
+++ authenticaition/src/components/singin.js
@@ -8,7 +8,7 @@ import React, {
 
 var Button = require('./common/button');
 var Auth0Lock = require('react-native-lock-ios');
-var lock = new Auth0Lock({clientId: "GX3rgSNmSA9TB5zwsgWcwdhJL5adA9A7", domain: "coldevtest.eu.auth0.com"});
+var lock = new Auth0Lock({clientId: "your key", domain: "your domain"});
 
 
 module.exports = React.createClass({
